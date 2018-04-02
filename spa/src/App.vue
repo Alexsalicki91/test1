c82e961438d30013628686e2ee20b7977f7384d3
Alex Salicki <salicki.alex@gmail.com>
2018-04-02 23:12:12 +0200

    Slider

---
+++ spa/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+		<nav class="demos">
+  <!-- the router outlet, where all matched components would ber viewed -->
+  <router-link v-bind:to="'/'">Home</router-link>
+<router-link v-bind:to="'/about'">About</router-link>
+</nav>
+    <router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+
+<!-- styling for the component -->
+<style>
+#app {
+
+}
+</style>
