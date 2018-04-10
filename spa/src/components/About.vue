f7c29d00f2cdb827819f570c457146fc6374bdba
Alex Salicki <salicki.alex@gmail.com>
2018-04-10 22:53:40 +0200

    Animation

--- spa/src/components/About.vue
+++ spa/src/components/About.vue
@@ -1,22 +1,81 @@
 <template>
   <div id="about">
-  When you have a great story about how your product or service was built to change lives, share it. The "About Us" page is a great place for it to live, too. Good stories humanize your brand, providing context and meaning for your product. What’s more, good stories are sticky -- which means people are more likely to connect with them and pass them on.
+		<section class="section-2">
+    <transition :duration="1000" name="fade">
+		<div class="section__about">
+			<h2 class="section__title_about">Hey,</h2>
+			<p class="section__description">Enim funeribus nemo ad has Scironis Scironis nemo quae quae ad pilatorum ut praerupta sed navem contigui quae navigabant deinde sunt contigui letalia letalia navem sed navigabant has appulit has.
+				<span class="section__description-inner"></span></p>
+		</div>
+		</transition>
+
+
+		<div class ="section__contact">
+			<h1 class="section__title_contact">Personal Contact :</h1>
+			<p class="section__description_contact">salicki.alex@gmail.com<br>+33632913360
+				<span class="section__description-inner"></span></p>
+		</div>
+
+
+		<div class="section__info">
+			<p class="section__description_info">Coded and Designed by Alex Salicki
+				<span class="section__description-inner"></span></p>
+		</div>
+		<div class="section__about_me">
+			<p class="section__description_about_me">About me
+				<span class="section__description-inner"></span></p>
+		</div>
+		<div class="letter-a">A</div>
+		<div class="letter-b">B</div>
+		<div class="letter-o">O</div>
+	</section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'about'
-}
+  name: 'about',
+
+};
 </script>
 <!-- styling for the component -->
 <style>
-#about {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+
+
+
+	.fade-enter-active {
+		animation: coming 1s;
+	  animation-delay: .5s;
+		opacity: 0;
+	}
+
+	.fade-leave-active {
+		animation: going 1s;
+
+	}
+
+
+	@keyframes going {
+		from {
+			transform: translateX(0);
+		}
+		to {
+			transform: translateX(-50px);
+			opacity:0;
+		}
+	}
+
+	@keyframes coming {
+		from {
+			transform: translateX(-50px);
+			opacity: 0;
+		}
+		to {
+			transform: translateX(0px);
+			opacity: 1;
+		}
+	}
+
 </style>
