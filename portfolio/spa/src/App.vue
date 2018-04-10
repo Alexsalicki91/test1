16d978e2ce00418d7a043727061fe9ed19e6b09d
Alex Salicki <salicki.alex@gmail.com>
2018-04-10 23:02:03 +0200

    portoflio

---
+++ portfolio/spa/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="app">
+		<div class="Grid">
+			<div class="Grid-cell u-size12 u-offset2">
+				<div class="Stripes-container">
+					<div class="Stripes-item"></div>
+				</div>
+			</div>
+
+		</div>
+		<div class = "horizontal">
+			<div class="stripes-horizontal_1"></div>
+			<div class="stripes-horizontal_2"></div>
+			<div class="stripes-horizontal_3"></div>
+			<div class="stripes-horizontal_4"></div>
+			<div class="stripes-horizontal_5"></div>
+			<div class="stripes-horizontal_6"></div>
+
+			<nav class="nav-top">
+			<!-- the router outlet, where all matched components would ber viewed -->
+			<router-link v-bind:to="'/'" style="padding:10px ; font-size:12px">+Projects</router-link>
+			<router-link v-bind:to="'/about'"style="padding:10px ; font-size:12px">About</router-link>
+			</nav>
+			<nav class="network">
+			<!-- the router outlet, where all matched components would ber viewed -->
+			<router-link v-bind:to="'/'" style="padding:10px ; font-size:12px">Dribbble</router-link>
+			<router-link v-bind:to="'/about'"style="padding:10px ; font-size:12px">Linkedin</router-link>
+			</nav>
+			<nav class="logo">
+			<!-- the router outlet, where all matched components would ber viewed -->
+			<router-link v-bind:to="'/'" style="padding:10px ; font-size:17px">Alex S.</router-link>
+			</nav>
+	</div>
+
+  <transition name="router-anim">
+    <router-view></router-view>
+  </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+
+<!-- styling for the component -->
+<style>
+
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+
+
+.router-anim-enter-active {
+	animation: coming 1s;
+  animation-delay: .5s;
+	opacity: 0;
+}
+
+.router-anim-leave-active {
+	animation: going 1s;
+
+}
+
+@keyframes going {
+	from {
+		transform: translateX(0);
+	}
+	to {
+		transform: translateX(-50px);
+		opacity:0;
+	}
+}
+
+@keyframes coming {
+	from {
+		transform: translateX(-50px);
+		opacity: 0;
+	}
+	to {
+		transform: translateX(0px);
+		opacity: 1;
+	}
+}
+
+
+</style>
---
+++ portfolio/spa/src/components/About.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="about">
+		<section class="section-2">
+    <transition :duration="1000" name="fade">
+		<div class="section__about">
+			<h2 class="section__title_about">Hey,</h2>
+			<p class="section__description">Enim funeribus nemo ad has Scironis Scironis nemo quae quae ad pilatorum ut praerupta sed navem contigui quae navigabant deinde sunt contigui letalia letalia navem sed navigabant has appulit has.
+				<span class="section__description-inner"></span></p>
+		</div>
+		</transition>
+
+
+		<div class ="section__contact">
+			<h1 class="section__title_contact">Personal Contact :</h1>
+			<p class="section__description_contact">salicki.alex@gmail.com<br>+33632913360
+				<span class="section__description-inner"></span></p>
+		</div>
+
+
+		<div class="section__info">
+			<p class="section__description_info">Coded and Designed by Alex Salicki
+				<span class="section__description-inner"></span></p>
+		</div>
+		<div class="section__about_me">
+			<p class="section__description_about_me">About me
+				<span class="section__description-inner"></span></p>
+		</div>
+		<div class="letter-a">A</div>
+		<div class="letter-b">B</div>
+		<div class="letter-o">O</div>
+	</section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+
+};
+</script>
+<!-- styling for the component -->
+<style>
+
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+
+
+
+	.fade-enter-active {
+		animation: coming 1s;
+	  animation-delay: .5s;
+		opacity: 0;
+	}
+
+	.fade-leave-active {
+		animation: going 1s;
+
+	}
+
+
+	@keyframes going {
+		from {
+			transform: translateX(0);
+		}
+		to {
+			transform: translateX(-50px);
+			opacity:0;
+		}
+	}
+
+	@keyframes coming {
+		from {
+			transform: translateX(-50px);
+			opacity: 0;
+		}
+		to {
+			transform: translateX(0px);
+			opacity: 1;
+		}
+	}
+
+</style>
