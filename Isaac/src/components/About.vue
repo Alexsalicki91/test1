f0ab243cc47a73e4ec8ade7daacb160287af1274
Alexsalicki91 <salicki.alex@gmail.com>
2018-11-08 22:52:47 +0100

    Isaac

---
+++ Isaac/src/components/About.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="about">
+		<section class="section-2">
+
+		<div class="section__about">
+			<h2 class="section__title_project">Hey, I'm Isaac Salehy</h2>
+			<p class="section__description">Je suis actuellement en troisième année dans l’axe Animation 3D à l’Institut de l’internet et du Multimédia à la Défense. <br>Je suis passionné depuis longtemps par le cinéma d’animation, le multimédia ainsi que le jeu vidéo et je suis donc cette formation depuis 2016.
+Ma licence dans l’axe Animation 3D m’apporte de solides compétences de modélisation et d’animation notamment grâce à la maitrise de différents logiciels (3Ds Max, Maya, Zbrush) mais également dans divers moteurs temps réels tels que Unity et Unreal Engine. <br>J’aime la modélisation et le texturing mais je préfère la gestion des projets.
+Après avoir acquis des capacités techniques en 3D, je souhaite donc poursuivre mes études avec un mastère en Gestion de Production 3D. A long terme, je voudrais travailler dans le cinéma d’animation en tant que chargé de production.
+
+				<span class="section__description-inner"><a href="/static/cv_isaac_salehy.pdf" download target=_blank style="padding:10px ; font-size:20px ; color:#fff; margin-top:auto; margin-bottom:auto ; text-decoration: underline;"><br>Download my CV
+					</a></span></p>
+
+		</div>
+
+
+		<div class ="section__contact">
+			<h1 class="section__title_contact">Personal Contact :</h1>
+			<p class="section__description_contact">salehy.isaac@gmail.com<br>+33603208197
+				<span class="section__description-inner"></span></p>
+		</div>
+
+
+		<div class="section__info">
+			<p class="section__description_info">Coded and Designed by Alex Salicki
+				<span class="section__description-inner"></span></p>
+		</div>
+		<div class="section__about_me">
+			<p class="section__description_about_me">About me
+				<span class="section__description-inner"></span></p>
+		</div>
+		<div class="loader-img">
+			<img src=""/>
+		</div>
+		<div class="letter-a">A</div>
+		<div class="letter-b">B</div>
+		<div class="letter-o">O</div>
+		<div class="letter-u">U</div>
+		<div class="letter-t">T</div>
+	</section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+	mounted() {
+
+		var about = new TimelineLite();
+
+		about
+
+		.from($('.letter-a'), 0.5, { autoAlpha: 0, ease:Power0.easeNone}, '2.5')
+		.from($('.section__contact'), 0.5, { marginLeft:30, autoAlpha: 0, ease:Power0.easeNone}, '2')
+		.from($('.section__description_about_me'), 0.5, { marginLeft:30, autoAlpha: 0, ease:Power0.easeNone}, '2')
+		.from($('.section__title_project'), 0.5, { x:-30, autoAlpha: 0, ease:Power0.easeNone}, '1.5')
+		.from($('.section__description'), 0.8, {  autoAlpha: 0, ease:Power2.easeIn}, '1.7')
+		.from($('.letter-b'), 0.5, { autoAlpha: 0, ease:Power0.easeNone}, '3')
+		.from($('.letter-o'), 0.5, { autoAlpha: 0, ease:Power0.easeNone}, '3.5')
+		.from($('.letter-u'), 0.5, { autoAlpha: 0, ease:Power0.easeNone}, '4')
+		.from($('.letter-t'), 0.5, { autoAlpha: 0, ease:Power0.easeNone}, '4.5')
+		.to($('.project'), 0.3, {  autoAlpha: 5, ease:Power0.easeNone}, '1.5')
+
+
+
+	}
+
+}
+</script>
+<!-- styling for the component -->
+<style>
+
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+
+
+.section__about {
+	animation-delay: 1.5s;
+}
+
+.pause-animation {
+	animation-play-state: paused;
+}
+
+</style>
---
+++ Isaac/src/components/Manners.vue
@@ -0,0 +1,208 @@
+<template>
+  <div id="manners is-loaded">
+
+		<!-- the router outlet, where all matched components would ber viewed -->
+		<nav class="back-nav">
+	  <div class="back">
+			<router-link v-bind:to="'/'" style="font-size:15px ; color:#fff">Back</router-link>
+		</div>
+	</nav>
+		<section class="section-project">
+		<div class="section__bg">Props</div>
+		<div class="section__project">
+			<h2 class="section__title_project">Hugo Boss Parfume</h2>
+			<div class="stripes-horizontal_project"></div>
+		</div>
+		<div class="section__info_launch">
+			<p class="section__description_work">Props
+				<span class="section__description-inner"></span></p>
+			</div>
+		<div class="section__info_work">
+			<p class="section__description_work">Work
+				<span class="section__description-inner">Props</span></p>
+			</div>
+			<div class="section__info_client">
+				<p class="section__description_work">School
+					<span class="section__description-inner"></span></p>
+				</div>
+				<div class="section__info_project">A PROPOS<br>DU PROJET
+					</div>
+					<div class="section__description_project">Projet dont l'objectif était de modéliser et texturer un parfum deja existant. Pour ce faire, j'ai choisi Boss by Hugo Boss et ai utilisé 3D Studio Max ainsi que Vray
+						</div>
+	</section>
+
+		<section class="section-content-project-1">
+			<div class="section__img-project">
+				<img src="/static/parfum_4.jpg">
+			</div>
+			</section>
+			<section class="section-content-project-2">
+				<div class="section__img-project">
+					<img src="/static/parfum_5.jpg">
+				</div>
+				</section>
+				<section class="section-content-project-2">
+					<div class="section__img-project">
+						<img src="/static/parfum_1.jpg">
+					</div>
+					</section>
+					<section class="section-content-project-2">
+						<div class="section__img-project">
+							<img src="/static/parfum_2.jpg">
+						</div>
+						</section>
+				<section class="section-content-project-5">
+					<div class="Next_project">
+						<router-link v-bind:to="'/projects/euroservice'" style="font-size:70px ; color:#fff; margin-bottom:auto; margin-top:auto;">Next</router-link>
+					</div>
+					<div class="Info_project">Project
+					<div class="Info_project_name">Mickey</div>
+					</div>
+				</section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'manners',
+	mounted() {
+
+
+
+		// init controller
+		(function () { // wait for document ready
+			// init
+			var controller = new ScrollMagic.Controller();
+
+			// get all slides
+			var slides = document.querySelectorAll(".section-content-project");
+
+			// create scene for every slide
+			for (var i=0; i<slides.length; i++) {
+				new ScrollMagic.Scene({
+						triggerElement: slides[i]
+					})
+					.setPin(slides[i])
+          .removeIndicators()
+					.addTo(controller);
+			}
+		});
+
+		// init controller
+		var controller = new ScrollMagic.Controller();
+
+		// build scene
+
+
+							new ScrollMagic.Scene({triggerElement: ".section-content-project-1"})
+										.setTween(".section-project", {x: "", y:"-25%", ease: Linear.easeNone})
+                    .removeIndicators()
+
+										.addTo(controller);
+
+										new ScrollMagic.Scene({triggerElement: ".section-content-project-2"})
+													.setTween(".section-content-project-1", {x: "", y:"-10%", ease: Linear.easeNone})
+													.removeIndicators()
+
+													.addTo(controller);
+													new ScrollMagic.Scene({triggerElement: ".section-content-project-1"})
+																.setTween(".section__bg", {x: "-40%", y:"", ease: Linear.easeNone})
+                                .removeIndicators()
+
+																.addTo(controller);
+
+									new ScrollMagic.Scene({triggerElement: ".section-content-project-3"})
+										.setTween(".section-content-project-2", {x: "", y:"-25%", ease: Linear.easeNone})
+										.removeIndicators()
+
+										.addTo(controller);
+										new ScrollMagic.Scene({triggerElement: ".section-content-project-4"})
+											.setTween(".section-content-project-3", {x: "", y:"-25%", ease: Linear.easeNone})
+                       .removeIndicators()
+
+											.addTo(controller);
+
+											var manners_vid = document.getElementById('manners-vid');
+
+											var controller = new ScrollMagic.Controller();
+
+											// build scene
+											var scene = new ScrollMagic.Scene({triggerElement: ".section-content-project-3"})
+															.addTo(controller)
+															.removeIndicators()
+
+															.on("enter", function () {
+																manners_vid.play();
+															})
+															.on("leave", function () {
+																manners_vid.currentTime=0;
+																manners_vid.pause();
+															})
+
+															var manners_vid_2 = document.getElementById('manners-vid_2');
+
+															var controller = new ScrollMagic.Controller();
+
+															// build scene
+															var scene = new ScrollMagic.Scene({triggerElement: ".section-content-project-4"})
+																			.addTo(controller)
+																			.removeIndicators()
+
+																			.on("enter", function () {
+																				manners_vid_2.play();
+																			})
+																			.on("leave", function () {
+																				manners_vid_2.currentTime=0;
+																				manners_vid_2.pause();
+																			})
+
+
+
+
+
+											imagesLoaded(document.querySelectorAll('#app'), () => {
+												document.body.classList.remove('loading');
+												// Init
+
+											});
+
+			var manners = new TimelineLite();
+
+			manners
+			.from($('.section__info_launch'), 0.5, { x:-10, autoAlpha: 0, ease:Power0.easeNone}, '1.5')
+			.from($('.section__info_work'), 0.5, { x:-10,  autoAlpha: 0, ease:Power0.easeNone}, '1.5')
+			.from($('.section__info_client'), 0.5, {x:-10,  autoAlpha: 0, ease:Power0.easeNone}, '1.5')
+			.from($('.section__info_project'), 0.5, { x:-10, autoAlpha: 0, ease:Power0.easeNone}, '1.5')
+			.from($('.section__description_project'), 0.6, { x:-10, autoAlpha: 0, ease:Power0.easeNone}, '1.5')
+			.from($('.section__bg'), 0.3, { x:-50, autoAlpha: 0, ease:Power0.easeNone}, '2.5')
+			.to($('.project'), 0.3, {  autoAlpha: 0, ease:Power0.easeNone}, '1')
+			.from($('.back'), 0.3, {  autoAlpha: 0, ease:Power0.easeNone}, '2')
+
+
+	}
+
+
+
+
+
+}
+
+</script>
+<!-- styling for the component -->
+<style>
+
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+
+
+
+.section__project {
+	animation-delay: 1s;
+}
+
+.pause-animation{
+	animation-play-state: paused;
+}
+
+
+
+</style>
